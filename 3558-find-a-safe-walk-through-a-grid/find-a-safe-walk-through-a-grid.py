3ac6a07dea79a562e15878b42b228b6ab189db96
Nandishsk-07 <skg109038@gmail.com>
2026-07-02 16:24:01 +0530

    Time: 87 ms (94.74%) | Memory: 12.5 MB (57.89%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
@@ -1,19 +1,22 @@
-from collections import deque
-from typing import List
-class Solution:
-    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+class Solution(object):
+    def findSafeWalk(self, grid, health):
+        """
+        :type grid: List[List[int]]
+        :type health: int
+        :rtype: bool
+        """
         m, n = len(grid), len(grid[0])
-        min_damage = [[float('inf')] * n for _ in range(m)]
+        min_damage = [[float('inf')] * n for _ in xrange(m)]
         start_damage = grid[0][0]
         min_damage[0][0] = start_damage
         q = deque([(0, 0)])
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]      
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         while q:
             r, c = q.popleft()
             if r == m - 1 and c == n - 1:
-                return (health - min_damage[r][c]) >= 1               
+                return (health - min_damage[r][c]) >= 1
             for dr, dc in directions:
-                nr, nc = r + dr, c + dc                
+                nr, nc = r + dr, c + dc
                 if 0 <= nr < m and 0 <= nc < n:
                     weight = grid[nr][nc]
                     next_damage = min_damage[r][c] + weight
@@ -22,6 +25,6 @@ class Solution:
                         if weight == 0:
                             q.appendleft((nr, nc))
                         else:
-                            q.append((nr, nc))                            
+                            q.append((nr, nc))
         return (health - min_damage[m - 1][n - 1]) >= 1
         
