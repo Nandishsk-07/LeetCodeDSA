11ff91ec05bdc0e5dc5f67cb5cc0321a3a8c3b61
Nandishsk-07 <skg109038@gmail.com>
2026-07-04 16:29:42 +0530

    Time: 274 ms (83.02%) | Memory: 65.2 MB (88.68%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -1,22 +1,25 @@
-from collections import deque
-from typing import List
-class Solution:
-    def minScore(self, n: int, roads: List[List[int]]) -> int:
-        adj = [[] for _ in range(n + 1)]
+class Solution(object):
+    def minScore(self, n, roads):
+        """
+        :type n: int
+        :type roads: List[List[int]]
+        :rtype: int
+        """
+        adj = [[] for _ in xrange(n + 1)]
         for u, v, d in roads:
             adj[u].append((v, d))
             adj[v].append((u, d))
         q = deque([1])
         visited = [False] * (n + 1)
-        visited[1] = True      
-        min_score = float('inf')      
+        visited[1] = True
+        min_score = float('inf')
         while q:
-            u = q.popleft()           
+            u = q.popleft()
             for v, d in adj[u]:
                 if d < min_score:
-                    min_score = d                   
+                    min_score = d
                 if not visited[v]:
                     visited[v] = True
-                    q.append(v)                   
+                    q.append(v)
         return min_score
         
