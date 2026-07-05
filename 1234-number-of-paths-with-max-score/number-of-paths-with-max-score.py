12d52717efc7e809eaef0165afed78ba23e23ede
Nandishsk-07 <skg109038@gmail.com>
2026-07-05 23:10:18 +0530

    Time: 140 ms (33.33%) | Memory: 13.9 MB (11.11%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.py
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.py
@@ -1,21 +1,24 @@
-from typing import List
-class Solution:
-    def pathsWithMaxScore(self, board: List[str]) -> List[int]:
+class Solution(object):
+    def pathsWithMaxScore(self, board):
+        """
+        :type board: List[str]
+        :rtype: List[int]
+        """
         n = len(board)
         MOD = 10**9 + 7
-        dp = [[[0, 0] for _ in range(n)] for _ in range(n)]
+        dp = [[[0, 0] for _ in xrange(n)] for _ in xrange(n)]
         dp[n-1][n-1] = [0, 1]
-        for r in range(n - 1, -1, -1):
-            for c in range(n - 1, -1, -1):
+        for r in xrange(n - 1, -1, -1):
+            for c in xrange(n - 1, -1, -1):
                 if r == n - 1 and c == n - 1:
                     continue
                 if board[r][c] == 'X':
-                    continue                  
+                    continue
                 max_s, paths = -1, 0
                 directions = [(r + 1, c), (r, c + 1), (r + 1, c + 1)]
                 for nr, nc in directions:
                     if nr < n and nc < n and dp[nr][nc][1] > 0:
-                        prev_score, prev_paths = dp[nr][nc]         
+                        prev_score, prev_paths = dp[nr][nc]
                         if prev_score > max_s:
                             max_s = prev_score
                             paths = prev_paths
