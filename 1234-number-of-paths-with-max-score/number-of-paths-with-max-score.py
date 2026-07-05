00163e5d7a6c5a11a809b106fed4bb66497e05b9
Nandishsk-07 <skg109038@gmail.com>
2026-07-05 22:57:42 +0530

    Time: 99 ms (80.73%) | Memory: 20.6 MB (43.12%) - LeetSync

---
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.py
@@ -0,0 +1,28 @@
+from typing import List
+class Solution:
+    def pathsWithMaxScore(self, board: List[str]) -> List[int]:
+        n = len(board)
+        MOD = 10**9 + 7
+        dp = [[[0, 0] for _ in range(n)] for _ in range(n)]
+        dp[n-1][n-1] = [0, 1]
+        for r in range(n - 1, -1, -1):
+            for c in range(n - 1, -1, -1):
+                if r == n - 1 and c == n - 1:
+                    continue
+                if board[r][c] == 'X':
+                    continue                  
+                max_s, paths = -1, 0
+                directions = [(r + 1, c), (r, c + 1), (r + 1, c + 1)]
+                for nr, nc in directions:
+                    if nr < n and nc < n and dp[nr][nc][1] > 0:
+                        prev_score, prev_paths = dp[nr][nc]         
+                        if prev_score > max_s:
+                            max_s = prev_score
+                            paths = prev_paths
+                        elif prev_score == max_s:
+                            paths = (paths + prev_paths) % MOD
+                if max_s != -1:
+                    current_value = 0 if board[r][c] == 'E' else int(board[r][c])
+                    dp[r][c] = [max_s + current_value, paths]
+        return dp[0][0] if dp[0][0][1] > 0 else [0, 0]
+        
