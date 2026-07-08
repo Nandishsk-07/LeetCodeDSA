9371faec83ff95a013f9167b8bc23c220ad8ba1b
Nandishsk-07 <skg109038@gmail.com>
2026-07-08 16:42:56 +0530

    Time: 694 ms (75.00%) | Memory: 52.3 MB (87.50%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.py
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -1,15 +1,19 @@
-from typing import List
-class Solution:
-    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
+class Solution(object):
+    def sumAndMultiply(self, s, queries):
+        """
+        :type s: str
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
         m = len(s)
-        MOD = 10**9 + 7
+        MOD = 10**9 + 7       
         pow10 = [1] * (m + 1)
-        for i in range(1, m + 1):
-            pow10[i] = (pow10[i - 1] * 10) % MOD
+        for i in xrange(1, m + 1):
+            pow10[i] = (pow10[i - 1] * 10) % MOD           
         pref_nonzero_cnt = [0] * (m + 1)
         pref_digit_sum = [0] * (m + 1)
-        pref_x = [0] * (m + 1)       
-        for i in range(m):
+        pref_x = [0] * (m + 1)        
+        for i in xrange(m):
             digit = int(s[i])
             if digit != 0:
                 pref_nonzero_cnt[i + 1] = pref_nonzero_cnt[i] + 1
@@ -18,14 +22,14 @@ class Solution:
             else:
                 pref_nonzero_cnt[i + 1] = pref_nonzero_cnt[i]
                 pref_digit_sum[i + 1] = pref_digit_sum[i]
-                pref_x[i + 1] = pref_x[i]               
+                pref_x[i + 1] = pref_x[i]                
         ans = []
         for L, R in queries:
             digit_sum = pref_digit_sum[R + 1] - pref_digit_sum[L]
-            num_nonzero = pref_nonzero_cnt[R + 1] - pref_nonzero_cnt[L]            
+            num_nonzero = pref_nonzero_cnt[R + 1] - pref_nonzero_cnt[L]           
             if num_nonzero == 0:
                 ans.append(0)
-                continue
+                continue              
             x = (pref_x[R + 1] - pref_x[L] * pow10[num_nonzero]) % MOD
             score = (x * digit_sum) % MOD
             ans.append(score)           
