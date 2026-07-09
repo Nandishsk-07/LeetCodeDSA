712d7421c02422174970b85317af7f2c0d427710
Nandishsk-07 <skg109038@gmail.com>
2026-07-09 23:26:10 +0530

    Time: 101 ms (72.73%) | Memory: 41.8 MB (100.00%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
@@ -1,13 +1,20 @@
-from typing import List
-class Solution:
-    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+class Solution(object):
+    def pathExistenceQueries(self, n, nums, maxDiff, queries):
+        """
+        :type n: int
+        :type nums: List[int]
+        :type maxDiff: int
+        :type queries: List[List[int]]
+        :rtype: List[bool]
+        """
         component_id = [0] * n
-        current_id = 0
-        for i in range(1, n):
+        current_id = 0      
+        for i in xrange(1, n):
             if nums[i] - nums[i - 1] > maxDiff:
                 current_id += 1
-            component_id[i] = current_id
+            component_id[i] = current_id          
         ans = []
         for u, v in queries:
             ans.append(component_id[u] == component_id[v])          
         return ans
+        
