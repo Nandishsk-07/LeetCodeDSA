fc88d81112ac1fb13f5e09ec8841be985c6de96f
Nandishsk-07 <skg109038@gmail.com>
2026-07-10 16:15:37 +0530

    Time: 2538 ms (100.00%) | Memory: 78.5 MB (100.00%) - LeetSync

--- 3852-path-existence-queries-in-a-graph-ii/path-existence-queries-in-a-graph-ii.py
+++ 3852-path-existence-queries-in-a-graph-ii/path-existence-queries-in-a-graph-ii.py
@@ -1,21 +1,26 @@
-from typing import List
-class Solution:
-    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[int]:
-        LOG = 18 
-        sorted_pairs = sorted((val, idx) for idx, val in enumerate(nums))
+class Solution(object):
+    def pathExistenceQueries(self, n, nums, maxDiff, queries):
+        """
+        :type n: int
+        :type nums: List[int]
+        :type maxDiff: int
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        sorted_pairs = sorted((nums[i], i) for i in xrange(n))
         orig_to_sorted = [0] * n
-        for sorted_idx, (val, orig_idx) in enumerate(sorted_pairs):
-            orig_to_sorted[orig_idx] = sorted_idx
-        st = [[0] * LOG for _ in range(n)]
-        
+        for sorted_idx, pair in enumerate(sorted_pairs):
+            orig_to_sorted[pair[1]] = sorted_idx
+        LOG = 18
+        st = [[0] * LOG for _ in xrange(n)]
         r = 0
-        for i in range(n):
+        for i in xrange(n):
             r = max(r, i)
             while r + 1 < n and sorted_pairs[r + 1][0] - sorted_pairs[i][0] <= maxDiff:
                 r += 1
             st[i][0] = r
-        for j in range(1, LOG):
-            for i in range(n):
+        for j in xrange(1, LOG):
+            for i in xrange(n):
                 st[i][j] = st[st[i][j - 1]][j - 1]
         ans = []
         for u, v in queries:
@@ -23,19 +28,18 @@ class Solution:
             b = orig_to_sorted[v]
             if a > b:
                 a, b = b, a
-                
             if a == b:
                 ans.append(0)
-                continue              
+                continue
             curr = a
             steps = 0
-            for j in range(LOG - 1, -1, -1):
+            for j in xrange(LOG - 1, -1, -1):
                 if st[curr][j] < b:
                     curr = st[curr][j]
                     steps += (1 << j)
             if st[curr][0] >= b:
                 ans.append(steps + 1)
             else:
-                ans.append(-1)           
+                ans.append(-1)
         return ans
         
