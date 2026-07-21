363ff97d704805e36c80fa8bb850f0dd637fedfc
Nandishsk-07 <skg109038@gmail.com>
2026-07-21 19:48:35 +0530

    Time: 2139 ms (0.00%) | Memory: 22.4 MB (0.00%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.py
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.py
@@ -1,5 +1,9 @@
-class Solution:
-    def maxActiveSectionsAfterTrade(self, s: str) -> int:
+class Solution(object):
+    def maxActiveSectionsAfterTrade(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         t = '1' + s + '1'
         blocks = []
         i = 0
@@ -9,14 +13,13 @@ class Solution:
             while j < n and t[j] == t[i]:
                 j += 1
             blocks.append((t[i], j - i))
-            i = j
+            i = j   
         total_ones = s.count('1')
         max_trade_gain = 0
-        for idx in range(1, len(blocks) - 1):
+        for idx in xrange(1, len(blocks) - 1):
             if blocks[idx][0] == '1':
                 if blocks[idx - 1][0] == '0' and blocks[idx + 1][0] == '0':
                     zero_gain = blocks[idx - 1][1] + blocks[idx + 1][1]
                     max_trade_gain = max(max_trade_gain, zero_gain)
-                    
         return total_ones + max_trade_gain
         
