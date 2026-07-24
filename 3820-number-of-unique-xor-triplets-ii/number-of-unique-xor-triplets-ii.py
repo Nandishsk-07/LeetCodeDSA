3c1b75eff721b1328311b3f546c2f2c672275c71
Nandishsk-07 <skg109038@gmail.com>
2026-07-24 15:49:58 +0530

    Time: 8453 ms (80.00%) | Memory: 12.8 MB (60.00%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.py
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.py
@@ -1,14 +1,18 @@
-class Solution:
-    def uniqueXorTriplets(self, nums: List[int]) -> int:
+class Solution(object):
+    def uniqueXorTriplets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         unique_nums = list(set(nums))
-        n = len(unique_nums)
+        n = len(unique_nums) 
         pair_xors = set()
-        for i in range(n):
-            for j in range(i, n):
-                pair_xors.add(unique_nums[i] ^ unique_nums[j])
+        for i in xrange(n):
+            for j in xrange(i, n):
+                pair_xors.add(unique_nums[i] ^ unique_nums[j]) 
         triplet_xors = set()
         for pair_val in pair_xors:
             for num in unique_nums:
-                triplet_xors.add(pair_val ^ num)
+                triplet_xors.add(pair_val ^ num)        
         return len(triplet_xors)
         
