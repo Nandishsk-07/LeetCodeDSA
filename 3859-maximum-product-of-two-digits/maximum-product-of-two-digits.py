cdad7d267505ec2529eb64ed175bad42764a1350
Nandishsk-07 <skg109038@gmail.com>
2026-07-25 14:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (16.08%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.py
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.py
@@ -1,5 +1,9 @@
-class Solution:
-    def maxProduct(self, n: int) -> int:
+class Solution(object):
+    def maxProduct(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         digits = sorted(str(n))
         return int(digits[-1]) * int(digits[-2])
         
