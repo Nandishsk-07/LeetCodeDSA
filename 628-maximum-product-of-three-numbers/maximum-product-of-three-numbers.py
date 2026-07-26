29bc3cd30cda7327b04f98df5f27f6fac523c7f7
Nandishsk-07 <skg109038@gmail.com>
2026-07-26 18:06:57 +0530

    Time: 14 ms (91.90%) | Memory: 13.1 MB (66.06%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
@@ -1,6 +1,9 @@
-from typing import List
-class Solution:
-    def maximumProduct(self, nums: List[int]) -> int:
+class Solution(object):
+    def maximumProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         nums.sort()
         option1 = nums[-1] * nums[-2] * nums[-3]
         option2 = nums[0] * nums[1] * nums[-1]
