dc68ff53b1753c2bd0029592a61c95e5f7ac63f0
Nandishsk-07 <skg109038@gmail.com>
2026-07-29 20:14:24 +0530

    Time: 755 ms (52.11%) | Memory: 19.7 MB (80.28%) - LeetSync

---
+++ 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.py
@@ -0,0 +1,55 @@
+import collections
+class Solution:
+    def __init__(self):
+        self.MAX = 10**6 + 1
+    def smallestPalindrome(self, s: str, k: int) -> str:
+        count = collections.Counter(s)
+        odd_count = sum(1 for freq in count.values() if freq % 2 == 1)
+        if odd_count > 1:
+            return ""
+        half_count = [0] * 26
+        mid_letter = ""
+        for char, freq in count.items():
+            half_count[ord(char) - ord('a')] = freq // 2
+            if freq % 2 == 1:
+                mid_letter = char
+        total_perm = self._count_arrangements(half_count)
+        if k > total_perm:
+            return ""
+        left_half = self._generate_left_half(half_count, k)   
+        return "".join(left_half) + mid_letter + "".join(reversed(left_half))
+    def _generate_left_half(self, half_count: list[int], k: int) -> list[str]:
+        half_len = sum(half_count)
+        left = []   
+        for _ in range(half_len):
+            for i, freq in enumerate(half_count):
+                if freq == 0:
+                    continue
+                    
+                half_count[i] -= 1
+                arrangements = self._count_arrangements(half_count)
+                
+                if arrangements >= k:
+                    left.append(chr(i + ord('a')))
+                    break
+                else:
+                    k -= arrangements
+                    half_count[i] += 1               
+        return left
+    def _count_arrangements(self, count: list[int]) -> int:
+        total = sum(count)
+        res = 1
+        for freq in count:
+            res *= self._nCk(total, freq)
+            if res >= self.MAX:
+                return self.MAX
+            total -= freq
+        return res
+    def _nCk(self, n: int, k: int) -> int:
+        res = 1
+        for i in range(1, min(k, n - k) + 1):
+            res = res * (n - i + 1) // i
+            if res >= self.MAX:
+                return self.MAX
+        return res
+        
