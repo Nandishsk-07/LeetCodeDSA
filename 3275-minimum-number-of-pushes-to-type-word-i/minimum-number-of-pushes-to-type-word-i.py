091a6bf9f7e659484f8d21090bce7a6b690e99b9
Nandishsk-07 <skg109038@gmail.com>
2026-07-30 10:30:25 +0530

    Time: 1 ms (25.00%) | Memory: 12.3 MB (59.62%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -1,8 +1,12 @@
-class Solution:
-    def minimumPushes(self, word: str) -> int:
+class Solution(object):
+    def minimumPushes(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
         n = len(word)
         pushes = 0
-        for i in range(n):
+        for i in xrange(n):
             pushes += (i // 8) + 1
         return pushes
         
