3b0a5f03065aebc7399d4fe97af8b4f0cb80b399
Nandishsk-07 <skg109038@gmail.com>
2026-08-03 22:51:20 +0530

    Time: 993 ms (76.69%) | Memory: 14.4 MB (98.50%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.py
+++ 1522-stone-game-iii/stone-game-iii.py
@@ -1,16 +1,19 @@
-from typing import List
-class Solution:
-    def stoneGameIII(self, stoneValue: List[int]) -> str:
+class Solution(object):
+    def stoneGameIII(self, stoneValue):
+        """
+        :type stoneValue: List[int]
+        :rtype: str
+        """
         n = len(stoneValue)
-        dp = [0] * (n + 1)       
-        for i in range(n - 1, -1, -1):
+        dp = [0] * (n + 1)   
+        for i in xrange(n - 1, -1, -1):
             max_advantage = float('-inf')
-            take_sum = 0
-            for k in range(1, 4):
+            take_sum = 0       
+            for k in xrange(1, 4):
                 if i + k - 1 < n:
                     take_sum += stoneValue[i + k - 1]
-                    max_advantage = max(max_advantage, take_sum - dp[i + k])                    
-            dp[i] = max_advantage           
+                    max_advantage = max(max_advantage, take_sum - dp[i + k])
+            dp[i] = max_advantage       
         if dp[0] > 0:
             return "Alice"
         elif dp[0] < 0:
