ab1091829c002f85fa8ed2c2f8af701984baef4a
Nandishsk-07 <skg109038@gmail.com>
2026-08-09 12:46:19 +0530

    Time: 144 ms (81.25%) | Memory: 13.8 MB (32.29%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.py
+++ 1240-stone-game-ii/stone-game-ii.py
@@ -1,20 +1,25 @@
-from functools import lru_cache
-from typing import List
-class Solution:
-    def stoneGameII(self, piles: List[int]) -> int:
+class Solution(object):
+    def stoneGameII(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: int
+        """
         n = len(piles)
         suffix_sum = [0] * (n + 1)
-        for i in range(n - 1, -1, -1):
-            suffix_sum[i] = suffix_sum[i + 1] + piles[i]
-        @lru_cache(maxsize=None)
-        def dp(i: int, m: int) -> int:
+        for i in xrange(n - 1, -1, -1):
+            suffix_sum[i] = suffix_sum[i + 1] + piles[i]    
+        memo = {} 
+        def dp(i, m):
             if i + 2 * m >= n:
                 return suffix_sum[i]
+            if (i, m) in memo:
+                return memo[(i, m)]
             max_stones = 0
-            for x in range(1, 2 * m + 1):
+            for x in xrange(1, 2 * m + 1):
                 opponent_score = dp(i + x, max(m, x))
                 current_score = suffix_sum[i] - opponent_score
                 max_stones = max(max_stones, current_score)
+            memo[(i, m)] = max_stones
             return max_stones
         return dp(0, 1)
         
