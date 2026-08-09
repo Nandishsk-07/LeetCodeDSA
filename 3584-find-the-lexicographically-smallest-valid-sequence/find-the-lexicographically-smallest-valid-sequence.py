bf7965258e10d0d1267684934804c42829018890
Nandishsk-07 <skg109038@gmail.com>
2026-08-09 12:32:19 +0530

    Time: 523 ms (55.32%) | Memory: 47.3 MB (68.08%) - LeetSync

---
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.py
@@ -0,0 +1,32 @@
+from typing import List
+class Solution:
+    def validSequence(self, word1: str, word2: str) -> List[int]:
+        m, n = len(word1), len(word2)
+        last = [-1] * (n + 1)
+        last[n] = m   
+        curr = m - 1
+        for j in range(n - 1, -1, -1):
+            curr = min(curr, last[j + 1] - 1)
+            while curr >= 0 and word1[curr] != word2[j]:
+                curr -= 1
+            last[j] = curr
+            curr -= 1   
+        seq = []
+        i = 0
+        used_mismatch = False
+        for j in range(n):
+            while i < m:
+                if word1[i] == word2[j]:
+                    seq.append(i)
+                    i += 1
+                    break
+                else:
+                    if not used_mismatch and last[j + 1] > i:
+                        seq.append(i)
+                        used_mismatch = True
+                        i += 1
+                        break
+                    else:
+                        i += 1
+        return seq if len(seq) == n else []
+        
