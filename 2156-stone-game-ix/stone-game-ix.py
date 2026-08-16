cfaad0153f8de4d5416478427c2aed2354a50452
Nandishsk-07 <skg109038@gmail.com>
2026-08-16 08:53:48 +0530

    Time: 43 ms (88.55%) | Memory: 30.7 MB (44.27%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        c = [0,0,0]
+        for stone in stones:
+            c[stone % 3] += 1
+        c0, c1, c2 = c[0], c[1], c[2]
+        if c0 % 2 == 0:
+            return c1 >= 1 and c2 >= 1
+        return abs(c1 - c2) > 2
+        
