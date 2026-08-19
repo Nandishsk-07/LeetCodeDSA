cf039db83205cc285f8561a412bf5ac395c553fc
Nandishsk-07 <skg109038@gmail.com>
2026-08-19 16:36:17 +0530

    Time: 34 ms (71.21%) | Memory: 22.6 MB (78.41%) - LeetSync

---
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+        reserved_rows = defaultdict(int)
+        for r, c in reservedSeats:
+            if 2 <= c <= 9:
+                reserved_rows[r] |= (1 << c)
+        ans = 2 * n
+        LEFT_MASK = 60
+        RIGHT_MASK = 960
+        MID_MASK = 240
+        for mask in reserved_rows.values():
+            left_open = (mask & LEFT_MASK) == 0
+            right_open = (mask & RIGHT_MASK) == 0
+            mid_open = (mask & MID_MASK) == 0
+            if left_open and right_open:
+                continue
+            elif left_open or right_open or mid_open:
+                ans -= 1
+            else:
+                ans -= 2
+        return ans
+        
