30f50bd14a07a81e23830f577b2beb00716171c2
Nandishsk-07 <skg109038@gmail.com>
2026-08-19 16:32:55 +0530

    Time: 12 ms (22.92%) | Memory: 12.4 MB (54.17%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
@@ -1,13 +1,19 @@
-class Solution:
-    def largestInteger(self, nums: List[int], k: int) -> int:
+class Solution(object):
+    def largestInteger(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
         n = len(nums)
         subarray_count = defaultdict(int)
-        for i in range(n - k + 1):
+        for i in xrange(n - k + 1):
             unique_elements = set(nums[i:i + k])
             for val in unique_elements:
-                subarray_count[val] += 1
+                subarray_count[val] += 1       
         ans = -1
         for val, count in subarray_count.items():
             if count == 1:
                 ans = max(ans, val)
         return ans
+        
