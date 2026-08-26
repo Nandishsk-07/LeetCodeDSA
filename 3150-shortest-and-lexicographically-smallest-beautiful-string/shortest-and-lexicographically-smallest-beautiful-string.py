1677c92a987b8c2b8264101aa66bb7d962c9e114
Nandishsk-07 <skg109038@gmail.com>
2026-08-26 23:08:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (26.09%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,14 +1,19 @@
-class Solution:
-    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+class Solution(object):
+    def shortestBeautifulSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
         if s.count('1') < k:
             return ""   
         n = len(s)
         ans = ""
         ones_count = 0
         left = 0
-        for right in range(n):
+        for right in xrange(n):
             if s[right] == '1':
-                ones_count += 1
+                ones_count += 1  
             while ones_count == k:
                 if s[left] == '1':
                     curr = s[left:right + 1]
