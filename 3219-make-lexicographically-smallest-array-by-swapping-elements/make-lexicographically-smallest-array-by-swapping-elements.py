e276e7c61646ec28d9c5c3400b46e42a5d53b7e4
Nandishsk-07 <skg109038@gmail.com>
2026-08-29 13:14:47 +0530

    Time: 581 ms (46.15%) | Memory: 36.7 MB (92.31%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,17 +1,22 @@
-class Solution:
-    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+class Solution(object):
+    def lexicographicallySmallestArray(self, nums, limit):
+        """
+        :type nums: List[int]
+        :type limit: int
+        :rtype: List[int]
+        """
         n = len(nums)
-        sorted_pairs = sorted((val, idx) for idx, val in enumerate(nums))
+        sorted_pairs = sorted((nums[idx], idx) for idx in xrange(n))
         res = [0] * n
         i = 0
         while i < n:
             j = i + 1
             while j < n and sorted_pairs[j][0] - sorted_pairs[j - 1][0] <= limit:
-                j += 1
-            group_values = [sorted_pairs[k][0] for k in range(i, j)]
-            group_indices = sorted(sorted_pairs[k][1] for k in range(i, j))
-            for val, idx in zip(group_values, group_indices):
-                res[idx] = val
+                j += 1 
+            group_values = [sorted_pairs[k][0] for k in xrange(i, j)]
+            group_indices = sorted(sorted_pairs[k][1] for k in xrange(i, j))
+            for k in xrange(len(group_values)):
+                res[group_indices[k]] = group_values[k]
             i = j
         return res
         
