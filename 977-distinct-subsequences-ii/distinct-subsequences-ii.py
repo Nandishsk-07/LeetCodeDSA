21de50de25b1b9cc8e92ec0d27238a2824531540
Nandishsk-07 <skg109038@gmail.com>
2026-09-07 09:07:46 +0530

    Time: 14 ms (22.58%) | Memory: 12.3 MB (77.42%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.py
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.py
@@ -1,10 +1,13 @@
-class Solution:
-    def distinctSubseqII(self, s: str) -> int:
-        MOD = 1_000_000_007
+class Solution(object):
+    def distinctSubseqII(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        MOD = 10**9 + 7
         ends_with = [0] * 26
         for ch in s:
             idx = ord(ch) - ord('a')
             ends_with[idx] = (sum(ends_with) + 1) % MOD
         return sum(ends_with) % MOD
-    
         
