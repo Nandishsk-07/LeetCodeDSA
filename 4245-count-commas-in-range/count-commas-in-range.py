4ca3fa56d919c489aa614b38dd8eb5f986fa737e
Nandishsk-07 <skg109038@gmail.com>
2026-09-08 23:37:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (93.20%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.py
+++ 4245-count-commas-in-range/count-commas-in-range.py
@@ -1,4 +1,8 @@
-class Solution:
-    def countCommas(self, n: int) -> int:
+class Solution(object):
+    def countCommas(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         return max(0, n - 999)
         
