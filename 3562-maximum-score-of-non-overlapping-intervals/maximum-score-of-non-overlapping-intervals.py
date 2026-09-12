3c9a535b22c71e90b107df4316ac6fe94286f3fb
Nandishsk-07 <skg109038@gmail.com>
2026-09-12 21:20:27 +0530

    Time: 2409 ms (50.00%) | Memory: 95.2 MB (50.00%) - LeetSync

--- 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.py
+++ 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.py
@@ -1,18 +1,22 @@
-class Solution:
-    def maximumWeight(self, intervals: List[List[int]]) -> List[int]:
+class Solution(object):
+    def maximumWeight(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[int]
+        """
         n = len(intervals)
-        arr = sorted([(r, l, w, i) for i, (l, r, w) in enumerate(intervals)])
+        arr = sorted([(intervals[i][1], intervals[i][0], intervals[i][2], i) for i in xrange(n)])
         r_ends = [x[0] for x in arr]
         def is_better(cand, curr):
             if cand[0] != curr[0]:
                 return cand[0] > curr[0]
             return cand[1] < curr[1]
-        dp_table = [[(0, []) for _ in range(5)] for _ in range(n + 1)]
-        for i in range(1, n + 1):
+        dp_table = [[(0, []) for _ in xrange(5)] for _ in xrange(n + 1)]
+        for i in xrange(1, n + 1):
             r, l, w, orig_idx = arr[i - 1]
             p = bisect_left(r_ends, l)
-            for c in range(1, 5):
-                best_state = dp_table[i - 1][c]
+            for c in xrange(1, 5):
+                best_state = dp_table[i - 1][c]   
                 prev_score, prev_indices = dp_table[p][c - 1]
                 new_score = prev_score + w
                 new_indices = sorted(prev_indices + [orig_idx])
@@ -21,7 +25,7 @@ class Solution:
                     best_state = cand_state
                 dp_table[i][c] = best_state
         ans = (0, [])
-        for c in range(1, 5):
+        for c in xrange(1, 5):
             if is_better(dp_table[n][c], ans):
                 ans = dp_table[n][c]
         return ans[1]
