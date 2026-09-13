0f741147a6ba9ea195b2d07daabdfa6bec71076a
Nandishsk-07 <skg109038@gmail.com>
2026-09-13 13:43:27 +0530

    Time: 394 ms (76.12%) | Memory: 12.7 MB (35.82%) - LeetSync

--- 864-image-overlap/image-overlap.py
+++ 864-image-overlap/image-overlap.py
@@ -1,8 +1,13 @@
-class Solution:
-    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+class Solution(object):
+    def largestOverlap(self, img1, img2):
+        """
+        :type img1: List[List[int]]
+        :type img2: List[List[int]]
+        :rtype: int
+        """
         n = len(img1)
-        ones1 = [(r, c) for r in range(n) for c in range(n) if img1[r][c] == 1]
-        ones2 = [(r, c) for r in range(n) for c in range(n) if img2[r][c] == 1]
+        ones1 = [(r, c) for r in xrange(n) for c in xrange(n) if img1[r][c] == 1]
+        ones2 = [(r, c) for r in xrange(n) for c in xrange(n) if img2[r][c] == 1]
         if not ones1 or not ones2:
             return 0
         shift_counts = Counter()
