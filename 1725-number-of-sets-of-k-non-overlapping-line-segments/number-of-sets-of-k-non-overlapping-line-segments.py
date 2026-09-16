91d7a87bfac83a53b69c85fecab46c7ee38b1e7c
Nandishsk-07 <skg109038@gmail.com>
2026-09-16 16:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (42.86%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,5 +1,19 @@
-class Solution:
-    def numberOfSets(self, n: int, k: int) -> int:
-        MOD = 1_000_000_007
-        return math.comb(n + k - 1, 2 * k) % MOD
+class Solution(object):
+    def numberOfSets(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        N = n + k - 1
+        R = 2 * k
+        if R > N or R < 0:
+            return 0
+        num = 1
+        den = 1
+        for i in xrange(1, R + 1):
+            num = (num * (N - i + 1)) % MOD
+            den = (den * i) % MOD
+        return (num * pow(den, MOD - 2, MOD)) % MOD
         
