cbd4c0ade3d8e8138d5df4256ddb81eaf43342f7
Nandishsk-07 <skg109038@gmail.com>
2026-09-20 15:26:19 +0530

    Time: 9 ms (71.59%) | Memory: 12.5 MB (22.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -1,7 +1,12 @@
-class Solution:
-    def reverseDegree(self, s: str) -> int:
-        return sum(
-            (i + 1) * (26 - (ord(ch) - ord('a')))
-            for i, ch in enumerate(s)
-        )
+class Solution(object):
+    def reverseDegree(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        total = 0
+        for i, ch in enumerate(s):
+            weight = 26 - (ord(ch) - ord('a'))
+            total += (i + 1) * weight
+        return total
         
