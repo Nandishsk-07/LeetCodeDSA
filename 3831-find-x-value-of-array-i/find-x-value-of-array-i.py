c11b52b19d779322ba35986fe92f39806f7500af
Nandishsk-07 <skg109038@gmail.com>
2026-09-21 22:33:00 +0530

    Time: 512 ms (66.67%) | Memory: 21.3 MB (0.00%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -1,16 +1,21 @@
-class Solution:
-    def resultArray(self, nums: List[int], k: int) -> List[int]:
+class Solution(object):
+    def resultArray(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
         ans = [0] * k
         dp = [0] * k
         for num in nums:
             new_dp = [0] * k
-            mod_val = num % k
+            mod_val = num % k 
             new_dp[mod_val] += 1
-            for r in range(k):
+            for r in xrange(k):
                 if dp[r]:
                     new_rem = (r * mod_val) % k
                     new_dp[new_rem] += dp[r]
-            for r in range(k):
+            for r in xrange(k):
                 ans[r] += new_dp[r]
             dp = new_dp
         return ans
