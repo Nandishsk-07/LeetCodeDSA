4e74db9b97edb8198ba124c64640f796c8852127
Nandishsk-07 <skg109038@gmail.com>
2026-09-25 23:39:42 +0530

    Time: 3 ms (79.78%) | Memory: 19.6 MB (56.83%) - LeetSync

---
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def braceExpansionII(self, expression: str) -> list[str]:
+        stack = []
+        cur_union = set()
+        cur_product = {""}
+        for ch in expression:
+            if ch.isalpha():
+                cur_product = {word + ch for word in cur_product}
+            elif ch == '{':
+                stack.append((cur_union, cur_product))
+                cur_union = set()
+                cur_product = {""}
+            elif ch == ',':
+                cur_union |= cur_product
+                cur_product = {""}
+            elif ch == '}':
+                inner_result = cur_union | cur_product
+                prev_union, prev_product = stack.pop()
+                cur_product = {p + w for p in prev_product for w in inner_result}
+                cur_union = prev_union
+        return sorted(list(cur_union | cur_product))
